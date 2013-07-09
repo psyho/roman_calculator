6ce9564b57d340c67c312b88a3df1f7d6b9f2da7
Adam Pohorecki <adam.pohorecki@llp.pl>
2013-07-09 15:48:32 +0200

    Initial checker implementation

---
+++ spec/checker_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+SERVER = 'localhost'
+
+class CalculatorRunner
+  def self.run(server, team_name)
+    new(server, team_name).run
+  end
+
+  attr_reader :server, :team_name
+
+  def initialize(server, team_name)
+  end
+
+  def run
+    send_results
+    printable_results
+  end
+
+  private
+
+  def printable_results
+    solved + [failed.first]
+  end
+
+  def results
+    examples.map do |x, y, expected|
+      actual = x.to_roman + y.to_roman
+      [x, y, expected, actual]
+    end
+  end
+
+  def examples
+    [["I", "I", "II"],
+     ["IV", "V", "IX"]]
+  end
+
+  def solved
+    results.select { |_, _, expected, actual| expected == actual }
+  end
+
+  def failed
+    results.reject { |_, _, expected, actual| expected == actual }
+  end
+
+  def send_results
+    puts "Team #{team_name} has solved #{solved.count} examples"
+  end
+end
+
+describe "Roman Calculator Checker" do
+  if TEAM_NAME
+    calculator_examples = CalculatorRunner.run(SERVER, TEAM_NAME)
+
+    calculator_examples.each do |x, y, expected, actual|
+      it "sums #{x} and #{y} to #{expected}" do
+        actual.should == expected
+      end
+    end
+  else
+    it "requires a team name" do
+      fail("Please fill in your team name in spec_helper.rb")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 require_relative "../lib/roman_numeral"
 require_relative "../lib/extensions"
+
+# FIXME: Provide your team name here
+TEAM_NAME = nil
